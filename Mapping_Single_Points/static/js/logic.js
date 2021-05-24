732052d884eb0d452d48d0bdc431e853256e62a8
mdabbous88 <77476333+mdabbous88@users.noreply.github.com>
2021-05-23 20:59:46 -0400

    Updated files

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -19,8 +19,9 @@ streets.addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
 let marker = L.marker([34.0522, -118.2437]).addTo(map);
-let circleMarker = L.circleMarker([34.0522, -118.2437], {radius:300, color:"black", fillColor:'#ffffa1'}).addTo(map);
+let circleMarker = L.circleMarker([34.0522, -118.2437], {radius:300, color:"red", fillColor:'#ffffa1'}).addTo(map);
 let circle = L.circle([34.0522, -118.2437], {
     radius: 100000,
     color: "yellow"
  }).addTo(map);
+ 
